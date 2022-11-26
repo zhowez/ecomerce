2a0a7a74859ca918e27ec5d78b876d7228b4509b
Zach Howes <zhowes14@icloud.com>
2022-11-26 17:42:40 -0500

    Added files for spring with security and MySQL abilities

---
+++ Backend/RM-API/src/main/java/com/roommatematching/rmapi/RmApiApplication.java
@@ -0,0 +1,13 @@
+package com.roommatematching.rmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RmApiApplication.class, args);
+    }
+
+}
