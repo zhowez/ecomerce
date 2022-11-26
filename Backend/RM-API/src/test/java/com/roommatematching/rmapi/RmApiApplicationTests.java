18a3cd198fb318fe1d308ba4ca18de3de15f593b
Zach Howes <zhowes14@icloud.com>
2022-11-26 18:35:45 -0500

    commented out test

--- Backend/RM-API/src/test/java/com/roommatematching/rmapi/RmApiApplicationTests.java
+++ Backend/RM-API/src/test/java/com/roommatematching/rmapi/RmApiApplicationTests.java
@@ -1,13 +1,13 @@
-package com.roommatematching.rmapi;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class RmApiApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.roommatematching.rmapi;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class RmApiApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
